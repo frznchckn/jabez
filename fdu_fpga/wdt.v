1e7c58734b7d710e39230906a0ac61f26cd2d542
russell.friesenhahn <russell.friesenhahn@9ce54e90-fae7-11dd-bc25-5f29055a2a2d>
2009-03-28 23:09:22 +0000

    M    wdt.v
    -changed everything to be on posedge clk triggered

--- fdu_fpga/wdt.v
+++ fdu_fpga/wdt.v
@@ -4,16 +4,16 @@ module wdt (
             input [2:0] fdu,
             output reg health);
    
-   reg [3:0]   state;
-   reg [3:0]   next_state;
+   reg [3:0]           state;
+   reg [3:0]           next_state;
 
    //actual WDT count
-   reg [23:0]  count;
-   reg         clear;
+   reg [23:0]          count;
+   reg                 clear;
    
-   parameter TIMEOUT = 6500000;
-// this for sim
-//   parameter TIMEOUT = 6000;
+   //   parameter TIMEOUT = 6500000;
+   // this for sim
+   parameter TIMEOUT = 6100;
    
    parameter IDLE = 4'b1110;
    parameter ZERO = 4'b0000;
@@ -34,6 +34,7 @@ module wdt (
         if (reset)
           begin
              count <= 0;
+             next_state <= IDLE;
              state <= IDLE;
              health <= UNHEALTHY;
              clear <= 0;
@@ -50,13 +51,13 @@ module wdt (
                   health <= UNHEALTHY;
                   count <= 0;
                   clear <= 0;
-                  state <= IDLE;
+                  next_state <= IDLE;
                end
              else if (state == SIX)
                begin
                   clear <= 1;
                end
-             else if (state == SEVEN)
+             else if (state == SEVEN && next_state != IDLE && next_state != ERROR)
                begin
                   
                   health <= HEALTHY;
@@ -66,166 +67,169 @@ module wdt (
                        count <= 0;
                        clear <= 0;
                     end
+               end // if (state == SEVEN)
+
+             next_state = state;
+             
+             case (state)
+
+               IDLE : begin
+                  if (fdu == 3'b000)
+                    begin
+                       next_state = ZERO;
+                    end
+                  else
+                    begin
+                       next_state = IDLE;
+                    end
                end
+
+               ZERO : begin
+                  if (fdu == 3'b000)
+                    begin
+                       next_state = ZERO;
+                    end
+                  else if (fdu == 3'b001)
+                    begin
+                       next_state = ONE;
+                    end
+                  else
+                    begin
+                       next_state = ERROR;
+                    end
+               end // case: ZERO
+               
+               ONE : begin
+                  if (fdu == 3'b001)
+                    begin
+                       next_state = ONE;
+                    end
+                  else if (fdu == 3'b011)
+                    begin
+                       next_state = TWO;
+                    end
+                  else
+                    begin
+                       next_state = ERROR;
+                    end
+               end
+
+               TWO : begin
+                  if (fdu == 3'b011)
+                    begin
+                       next_state = TWO;
+                    end
+                  else if (fdu == 3'b010)
+                    begin
+                       next_state = THREE;
+                    end
+                  else
+                    begin
+                       next_state = ERROR;
+                    end
+               end
+
+               THREE : begin
+                  if (fdu == 3'b010)
+                    begin
+                       next_state = THREE;
+                    end
+                  else if (fdu == 3'b110)
+                    begin
+                       next_state = FOUR;
+                    end
+                  else
+                    begin
+                       next_state = ERROR;
+                    end
+               end
+
+               FOUR : begin
+                  if (fdu == 3'b110)
+                    begin
+                       next_state = FOUR;
+                    end
+                  else if (fdu == 3'b111)
+                    begin
+                       next_state = FIVE;
+                    end
+                  else
+                    begin
+                       next_state = ERROR;
+                    end
+               end
+
+               FIVE : begin
+                  if (fdu == 3'b111)
+                    begin
+                       next_state = FIVE;
+                    end
+                  else if  (fdu == 3'b101)
+                    begin
+                       next_state = SIX;
+                    end
+                  else
+                    begin
+                       next_state = ERROR;
+                    end
+               end
+               
+
+               SIX : begin
+                  if (fdu == 3'b101)
+                    begin
+                       next_state = SIX;
+                    end
+                  else if (fdu == 3'b100)
+                    begin
+                       next_state = SEVEN;
+                    end
+                  else
+                    begin
+                       next_state = ERROR;
+                    end
+               end
+
+               SEVEN : begin
+                  if (fdu == 3'b100)
+                    begin
+                       next_state = SEVEN;
+                    end
+                  else if (fdu == 3'b000)
+                    begin
+                       next_state = ZERO;
+                    end
+                  else
+                    begin
+                       next_state = ERROR;
+                    end
+               end
+
+               ERROR : begin
+                  if (fdu == 3'b000)
+                    begin
+                       next_state = ZERO;
+                    end
+                  else
+                    begin
+                       next_state = IDLE;
+                    end
+               end
+
+               default : begin
+                  next_state = state;
+               end
+
+             endcase
+             
+
           end // else: !if(reset)
      end // always @ (posedge clk or posedge reset)
-   
-   always @ (negedge clk)
+
+   always @ (posedge clk)
+     //   always @ (fdu)
      begin
-        
-        next_state = state;
-        
-        case (state)
-
-          IDLE : begin
-             if (fdu == 3'b000)
-               begin
-                  next_state = ZERO;
-               end
-             else
-               begin
-                  next_state = IDLE;
-               end
-          end
-
-          ZERO : begin
-             if (fdu == 3'b000)
-               begin
-                  next_state = ZERO;
-               end
-             else if (fdu == 3'b001)
-               begin
-                  next_state = ONE;
-               end
-             else
-               begin
-                  next_state = ERROR;
-               end
-          end // case: ZERO
-          
-          ONE : begin
-             if (fdu == 3'b001)
-               begin
-                  next_state = ONE;
-               end
-             else if (fdu == 3'b011)
-               begin
-                  next_state = TWO;
-               end
-             else
-               begin
-                  next_state = ERROR;
-               end
-          end
-
-          TWO : begin
-             if (fdu == 3'b011)
-               begin
-                  next_state = TWO;
-               end
-             else if (fdu == 3'b010)
-               begin
-                  next_state = THREE;
-               end
-             else
-               begin
-                  next_state = ERROR;
-               end
-          end
-
-          THREE : begin
-             if (fdu == 3'b010)
-               begin
-                  next_state = THREE;
-               end
-             else if (fdu == 3'b110)
-               begin
-                  next_state = FOUR;
-               end
-             else
-               begin
-                  next_state = ERROR;
-               end
-          end
-
-          FOUR : begin
-             if (fdu == 3'b110)
-               begin
-                  next_state = FOUR;
-               end
-             else if (fdu == 3'b111)
-               begin
-                  next_state = FIVE;
-               end
-             else
-               begin
-                  next_state = ERROR;
-               end
-          end
-
-          FIVE : begin
-             if (fdu == 3'b111)
-               begin
-                  next_state = FIVE;
-               end
-             else if  (fdu == 3'b101)
-               begin
-                  next_state = SIX;
-               end
-             else
-               begin
-                  next_state = ERROR;
-               end
-          end
-          
-
-          SIX : begin
-             if (fdu == 3'b101)
-               begin
-                  next_state = SIX;
-               end
-             else if (fdu == 3'b100)
-               begin
-                  next_state = SEVEN;
-               end
-             else
-               begin
-                  next_state = ERROR;
-               end
-          end
-
-          SEVEN : begin
-             if (fdu == 3'b100)
-               begin
-                  next_state = SEVEN;
-               end
-             else if (fdu == 3'b000)
-               begin
-                  next_state = ZERO;
-               end
-             else
-               begin
-                  next_state = ERROR;
-               end
-          end
-
-          ERROR : begin
-             if (fdu == 3'b000)
-               begin
-                  next_state = ZERO;
-               end
-             else
-               begin
-                  next_state = IDLE;
-               end
-          end
-
-          default : begin
-             next_state = state;
-          end
-
-          endcase
 
      end
-              
+   
 endmodule
